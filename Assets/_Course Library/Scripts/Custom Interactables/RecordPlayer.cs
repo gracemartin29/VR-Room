16443b6c2b3e4db6660e832971b7761d63cf7ef5
gracemartin29 <grace.martin@student.norwichuni.ac.uk>
2025-03-03 12:52:21 +0000

    psedocode

---
+++ Assets/_Course Library/Scripts/Custom Interactables/RecordPlayer.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.InputSystem.Controls;
+
+public class RecordPlayer : MonoBehaviour
+{
+    public float rotationSpeed = 33.3f;
+
+    // var socketGO = socket game object
+    public GameObject socket;
+    // var redRecordGO = record red game object
+    // var blueRecordGO = record blue game object
+
+    // var currentRecord = record currently attached to socketGO
+
+    // var socketAS = socket audio source
+    // var redRecordAF = record red audio file
+    // var blueRecordAF = record blue audio file
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+// decides and plays audio
+    public void PlayRecord()
+    {
+        // if (currentRecord = redRecordGO){
+        // socketAS = redRecordAF
+        // } else if (currentRecord = blueRecordGO){
+        // socketAS = blueRecordAF
+        // } else {
+        // socketAS = null
+        // }
+        // play_audio(socketAS)
+
+    }
+
+// stops audio
+    public void StopRecord()
+    {
+        // stop_audio(socketAS)
+        // socketAS = null
+    }
+
+// spinning record when playing
+    public void StartSpinObject()
+    {
+        // continously rotate on x-axis 
+        transform.Rotate(2*rotationSpeed*Time.deltaTime, 0, 0);
+    }
+
+    public void StopSpinObject()
+    {
+        // stop rotating
+        transform.Rotate(0,0,0);
+    }
+
+}
